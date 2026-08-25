cd17970a36ef87b5d5f8bc0470688f8b9af717f3
bdmobile0167 <bdmobile0167@gmail.com>
2026-08-25 10:17:56 +0800

    Delete supabase/migrations directory

--- supabase/migrations/20260821065047_bind_existing_bank_accounts.sql
+++
@@ -1,9 +0,0 @@
-update public.bank_accounts
-set ledger_account_id = account.id,
-    accounting_account_id = account.id
-from public.accounts account
-where account.code = '1102'
-  and (
-    bank_accounts.ledger_account_id is null
-    or bank_accounts.accounting_account_id is null
-  );
--- supabase/migrations/20260821072313_restrict_profile_helper_execution.sql
+++
@@ -1,5 +0,0 @@
-revoke execute on function public.get_my_role() from anon;
-revoke execute on function public.get_my_department() from anon;
-
-grant execute on function public.get_my_role() to authenticator, authenticated, service_role;
-grant execute on function public.get_my_department() to authenticator, authenticated, service_role;
